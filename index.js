4a5090f3613bde8101020898ba0d508a6359570f
Filip Eller <filip.eller@aalto.fi>
2022-08-23 14:54:00 +0300

    Add /help command.

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-const Uwuifier = require('uwuifier');
 
-const uwuifier = new Uwuifier.default();
+const Uwuifier = require('uwuifier');
+const uwuifier = new Uwuifier.default({
+  spaces: { faces: 0.125, actions: 0.075, stutters: 0.175 },
+  words: 1,
+  exclamations: 1,
+});
+
+/*
+DEFAULTS = {
+    spaces: { faces: 0.05, actions: 0.075, stutters: 0.1 },
+    words: 1,
+    exclamations: 1,
+}
+*/
 
 const { TOKEN } = process.env;
 if (!TOKEN) {
@@ -24,7 +36,11 @@ const sendMessage = async (text, chat_id) => {
     });
     return res;
   } catch (e) {
-    console.error('Error sending message: ', e.message);
+    console.error(
+      'Error sending message: ',
+      e.message,
+      e.response?.data?.description
+    );
     return null;
   }
 };
@@ -37,7 +53,11 @@ const answerInlineQuery = async (results, inline_query_id) => {
     });
     return res;
   } catch (e) {
-    console.log('Error answering inline query:', e.response?.data?.description);
+    console.error(
+      'Error answering inline query:',
+      e.message,
+      e.response?.data?.description
+    );
     return null;
   }
 };
@@ -47,33 +67,45 @@ app.get('/ping', (req, res) => {
 });
 
 app.post(URI, async (req, res) => {
-  console.log('update', req.body);
   const update = req.body;
 
   if (update.message) {
     const { message } = update;
-    const {
-      text,
-      chat: { id },
-    } = message;
+    const { text, chat } = message;
+    const { id, type } = chat;
 
-    if (text.startsWith('/')) {
-      if (text) {
-        const msg = text
-          .split(' ')
-          .map((word) => word.split('').reverse().join(''))
-          .join(' ');
+    let msg;
 
-        await sendMessage(msg, id);
-        return res.send();
+    if (text && type === 'private') {
+      if (
+        text.toLowerCase().includes('/start') ||
+        text.toLowerCase().includes('/help')
+      ) {
+        msg =
+          'This bot can help translate your messages to UwU language. ' +
+          'It works automatically, no need to add it anywhere. ' +
+          'Simply open any of your chats and type @UwUrawrbot + something in the message field. ' +
+          'Then tap on a result to send.\n\n' +
+          'For example, try typing \n' +
+          '@UwUrawrbot hello\n' +
+          'in this chat.\n\n' +
+          'Inline queries have a maximum length of 256 characters. ' +
+          'If you want to translate longer messages, just send the message here, ' +
+          'and the bot will reply with the translated version.';
+      } else {
+        msg = uwuifier.uwuifySentence(text);
       }
+      await sendMessage(msg, id);
+      return res.send();
     }
   }
 
   if (update.inline_query) {
     const { id: inline_query_id, query } = update.inline_query;
 
-    const uwuText = uwuifier.uwuifySentence(query) || 'UwU';
+    const uwuified = uwuifier.uwuifySentence(query);
+    const uwuText =
+      query.length >= 252 ? uwuified + ' *runs away*' : uwuified || 'UwU';
     const description =
       uwuText.length > 90 ? uwuText.slice(0, 90) + '...' : uwuText;
 
@@ -88,7 +120,6 @@ app.post(URI, async (req, res) => {
       input_message_content,
       description,
     };
-    console.log('result:', inlineQueryResult);
     answerInlineQuery([inlineQueryResult], inline_query_id);
   }
   res.send();
@@ -97,5 +128,3 @@ app.post(URI, async (req, res) => {
 app.listen(process.env.PORT || 5000, async () => {
   console.log('Listening on port', process.env.PORT || 5000);
 });
-
-// msg.split(' ').map((word) => word.split('').reverse().join('')).join(' ')
